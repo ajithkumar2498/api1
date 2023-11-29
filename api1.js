162f0512a785671efb4e25a64d242f9555275abc
ajithkumar2498 <ajibhc24@gmail.com>
2023-11-29 06:59:22 +0530

    updated

--- api1.js
+++ api1.js
@@ -5,30 +5,38 @@ function covidData(){
     request.open('GET',"https://data.covid19india.org/v4/min/data.min.json")
     request.send()
     request.onload = (function (){
-                  var data            
+                            
                if(request.status==200){
-                data= JSON.parse(request.response)
+                var data= JSON.parse(request.response)
                 resolve(data);
                }else{
                 reject("data")
                }  }
     )
   })
-   showCovidData.then((data)=>{console.log(data) 
-   let input = document.getElementsByClassName('datas')[0].value
-   console.log(input)
-   let getData= data.filter((state)=>{
-    state.input==data
-   })
-   console.log(getData)
-   let card =document.getElementsByClassName('card-body')
-   card.innerHTML=`<p class="card-text card-head">Covid-19 Data</p>
-   <p class="card-text1" >Confirmed:${getData.confirmed}</p>
-   <p class="card-text2">Deceased:</p>
-   <p class="card-text3">Recovered:</p>
-   <p class="card-text4">Tested:</p>
-   <p class="card-text5">Vaccinated1:</p>
-   <p class="card-text6">Vaccinated2:</p>`
+   showCovidData.then((data)=>{
+    console.log(data) 
+   let select = document.querySelector('.states-value').value
+  //  console.log(select)
+  //  let getData = data.forEach(element => {
+  //   element[key]===input
+  //  });
+    
+   
+  //  console.log(getData)
+   let card =document.querySelector('.card-body')
+   
+   card.innerHTML=`
+   <p class="card-text1" >Confirmed: &nbsp${data[select].total.confirmed}</p>
+   <p class="card-text2">Deceased: &nbsp${data[select].total.deceased}</p>
+   <p class="card-text3">Recovered: &nbsp${data[select].total.recovered}</p>
+   <p class="card-text4">Tested: &nbsp${data[select].total.tested}</p>
+   <p class="card-text5">Vaccinated1: &nbsp${data[select].total.vaccinated1}</p>
+   <p class="card-text6">Vaccinated2: &nbsp${data[select].total.vaccinated2}</p>`
+
+  // document.querySelector('.card-text1')
+
+  //  document.body.append('card')
  
   }).catch((err)=>{console.log(err)})
  }
